4a7943ebca7fd37eda9c88bf8a18c8578fc828db
Gustavo Gomez <ggo@topfloormarketing.net>
2020-02-17 10:56:21 -0600

    Fixed server playground and schema fetch and bumped up to 60fps

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill'
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
 import { ApolloServer, ApolloError } from 'apollo-server-express'
 import express from 'express'
 import { config } from 'dotenv'
@@ -91,7 +92,7 @@ const { jwtAuth } = Utils;
         }
         throw new ApolloError(`${error}`)
       },
-      debug: NODE_ENV !== 'production'
+      debug: PLAYGROUND
     })
 
     server.applyMiddleware({ app })
